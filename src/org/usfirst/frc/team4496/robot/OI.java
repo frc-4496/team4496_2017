5c3abdb7bb4e2aa6d5ed7817c1bce21d9cfc8217
swampy99 <levineboy624@gmail.com>
2017-02-12 18:39:16 -0700

    Began coding

---
+++ src/org/usfirst/frc/team4496/robot/OI.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team4496.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class OI {
+	public static Joystick controller = new Joystick(0);
+}
---
+++ src/org/usfirst/frc/team4496/robot/RobotMap.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team4496.robot;
+
+public class RobotMap {
+
+	public static int frontLeft = 0,
+				rearLeft = 1, 
+				frontRight = 2, 
+				rearRight = 3;
+	public static int launch = 4;
+}
